e6837fc451e3b3d9f429819efdaa2b7372083a8c
mamume <mmustafametwally@gmail.com>
2021-11-23 13:25:52 +0200

    initialize board model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Board(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    share = models.BooleanField(default=False)
+    description = models.TextField(default="", null=True)
+    cover_img = models.ImageField(upload_to="board_covers")
