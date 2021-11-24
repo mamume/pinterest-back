fe00ae9bdb991d45cfe1508f512dd48c6a3e0e74
mamume <mmustafametwally@gmail.com>
2021-11-24 21:16:08 +0200

    delete comments

--- board/models.py
+++ board/models.py
@@ -10,15 +10,8 @@ class Board(models.Model):
     cover_img = models.ImageField(upload_to="board/covers", null=True)
 
     # pins = models.ManyToManyField('Pin')
-    # categories = models.ManyToManyField('Category')
     collaborators = models.ManyToManyField('Collaborator')
     owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-
-    """
-        Other Relations:
-        1. section: 1 to many (sections) // to be done in sections
-        2. note: 1 to many (notes) // to be done in note
-    """
 
 
 class Collaborator(models.Model):
