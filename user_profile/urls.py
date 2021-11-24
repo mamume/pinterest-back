587081570b6612216d2d0f7dbaa4dea393477df2
mamume <mmustafametwally@gmail.com>
2021-11-24 16:18:46 +0200

    initialize views

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 url_patterns = [
-    path("<int:id>/", views.profile_detail, name="profile-detail")
+    path("<int:pk>/", views.profile_detail, name="profile-detail")
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from account.models import UserProfile
 
-# Create your views here.
+
+@api_view(['GET'])
+def profile_detail(request, pk):
+    profile = get_object_or_404(UserProfile, pk=pk)
+
+    return Response()
