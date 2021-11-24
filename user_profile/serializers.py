a91ce684a976602bb30533397c439f62bb1bacd6
mamume <mmustafametwally@gmail.com>
2021-11-24 16:26:52 +0200

    Finish profile_detail with serializer

---
+++ user_profile/serializers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db.models import fields
+from rest_framework import serializers
+from account.models import UserProfile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ["username", "first_name", "last_name",
+                  "followers_num", "following_num", "pins", "pins_num"]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from account.models import UserProfile
+from .serializers import ProfileSerializer
 
 
 @api_view(['GET'])
 def profile_detail(request, pk):
     profile = get_object_or_404(UserProfile, pk=pk)
+    serializer = ProfileSerializer(profile)
+    serializer.is_valid(raise_exception=True)
 
-    return Response()
+    return Response(serializer.data)
