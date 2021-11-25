db39fe37700a28dd2c11179fcf19b994e9f23e96
mamume <mmustafametwally@gmail.com>
2021-11-25 13:31:00 +0200

    fix view error

--- board/views.py
+++ board/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import BoardSerializer
+from .models import Board
 
 
 class BoardDetail(APIView):
     def get(self, request, pk):
-        board = get_object_or_404(pk=pk)
+        board = get_object_or_404(Board, pk=pk)
         serializer = BoardSerializer(board)
 
         return Response(serializer.data)
