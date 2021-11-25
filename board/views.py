255bf8313f11f49e059940812bcde98bde29045d
mamume <mmustafametwally@gmail.com>
2021-11-25 13:27:35 +0200

    finish get request

--- board/views.py
+++ board/views.py
@@ -1,6 +1,12 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import BoardSerializer
 
 
 class BoardDetail(APIView):
-    def get(self, request):
-        pass
+    def get(self, request, pk):
+        board = get_object_or_404(pk=pk)
+        serializer = BoardSerializer(board)
+
+        return Response(serializer.data)
