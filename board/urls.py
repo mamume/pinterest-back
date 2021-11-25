5bc74d1f2c8259711e9bedb0fbe10269deee61ce
mamume <mmustafametwally@gmail.com>
2021-11-26 01:43:22 +0200

    add basename to urls

--- board/urls.py
+++ board/urls.py
@@ -4,9 +4,10 @@ from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register('list', views.BoardViewSet, basename='board-list')
-router.register('collaborator', views.CollaboratorViewSet)
-router.register('note', views.NoteViewSet)
-router.register('section', views.SectionViewSet)
+router.register('collaborator', views.CollaboratorViewSet,
+                basename='collaborator-list')
+router.register('note', views.NoteViewSet, basename='note-list')
+router.register('section', views.SectionViewSet, basename='section-list')
 
 
 urlpatterns = router.urls
