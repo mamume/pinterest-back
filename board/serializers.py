7242e6dbefe923b8e195132ac48006af9d2221ad
mamume <mmustafametwally@gmail.com>
2021-11-25 13:25:42 +0200

    serializers

---
+++ board/serializers.py
@@ -0,0 +1,18 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Board, Collaborator
+
+
+class CollaboratorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collaborator
+        fields = '__all__'
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = ['title', 'share', 'description',
+                  'collaborators', 'cover_image']
+
+    collaborators = CollaboratorSerializer()
