be57bf0fb8a2072acf227c96a1257b29b14c62e3
mamume <mmustafametwally@gmail.com>
2021-11-26 02:28:20 +0200

    initialize get_following_count

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from account.models import UserProfile
+from account.models import UserProfile, UserFollowing
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -9,9 +9,12 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     # country_name = serializers.SerializerMethodField('get_country_name')
     full_name = serializers.SerializerMethodField('get_full_name')
-
+    following_count = serializers.SerializerMethodField('get_following_count')
     # def get_country_name(self, instance):
     #     return instance.country.name
 
     def get_full_name(self, instance: UserProfile):
         return f"{instance.first_name} {instance.last_name}"
+
+    def get_following_count(self, instance: UserProfile):
+        return UserFollowing.objects.filter()
