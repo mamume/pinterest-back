1998103b11b6574e621de99ca797c2999bf02a58
mamume <mmustafametwally@gmail.com>
2021-11-26 02:06:16 +0200

    add profile serializer

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -5,5 +5,4 @@ from account.models import UserProfile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ["username", "first_name", "last_name",
-                  "followers_num", "following_num", "pins", "pins_num"]
+        fields = '__all__'
