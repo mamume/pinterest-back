dc091cdcb8ddb44d2af894a71c10b590322935ec
mamume <mmustafametwally@gmail.com>
2021-11-26 02:20:12 +0200

    initialize profile serializer

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -5,4 +5,13 @@ from account.models import UserProfile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = '__all__'
+        fields = ['id', 'get_full_name', 'profile_pic', 'username']
+
+    # country_name = serializers.SerializerMethodField('get_country_name')
+    full_name = serializers.SerializerMethodField('get_full_name')
+
+    # def get_country_name(self, instance):
+    #     return instance.country.name
+
+    def get_full_name(self, instance: UserProfile):
+        return f"{instance.first_name} {instance.last_name}"
