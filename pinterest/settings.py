ee63d6c12363dbe3da8f9b6cdd1777dde5858863
mamume <mmustafametwally@gmail.com>
2021-11-26 18:46:08 +0200

    delete duplicated installed app

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -38,9 +38,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.sessions',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
