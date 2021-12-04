6fa133a7f616b8438347abdf25d0d56e8c40db33
Ahmed Saied <ahmed.saeed311294@gmail.com>
2021-12-04 22:34:46 +0200

    another fix

--- account/signals.py
+++ account/signals.py
@@ -20,23 +20,23 @@ acc_tok = generate_token()
 
 try:
     app = Application.objects.get(id=1)
+
+    @receiver(post_save, sender=UserProfile)
+    def signupToken(created, instance, *args, **kwargs):
+        if created:
+            if not instance.is_superuser:
+                refresh_token = RefreshToken.objects.create(user=instance, application=app, revoked=refresh_time(), token=ref_tok)
+                access_token = AccessToken.objects.create(
+                    user=instance,
+                    source_refresh_token=refresh_token, 
+                    application=app, 
+                    expires = access_time(),
+                    token=acc_tok,
+                    scope='read write'
+                )
+                RefreshToken.objects.filter(token=ref_tok).update(access_token=access_token)
 except:
     pass
-
-@receiver(post_save, sender=UserProfile)
-def signupToken(created, instance, *args, **kwargs):
-    if created:
-        if not instance.is_superuser:
-            refresh_token = RefreshToken.objects.create(user=instance, application=app, revoked=refresh_time(), token=ref_tok)
-            access_token = AccessToken.objects.create(
-                user=instance,
-                source_refresh_token=refresh_token, 
-                application=app, 
-                expires = access_time(),
-                token=acc_tok,
-                scope='read write'
-            )
-            RefreshToken.objects.filter(token=ref_tok).update(access_token=access_token)
 
 
             
