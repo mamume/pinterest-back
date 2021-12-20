c89118ded853de845cb3d810a993b60b8aa2f2c6
mamume <mmustafametwally@gmail.com>
2021-12-20 13:23:53 +0200

    add runtime file and static configurations

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -159,6 +159,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
