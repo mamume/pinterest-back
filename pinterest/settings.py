360268fdabfc80df5efe05bd7d79adda60e01c91
mamume <mmustafametwally@gmail.com>
2021-12-20 16:18:39 +0200

    change project_root

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -176,7 +176,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
