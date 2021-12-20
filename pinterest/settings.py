9038ae44569ae1b456f5121e8664b61f0ffe0680
mamume <mmustafametwally@gmail.com>
2021-12-20 13:33:54 +0200

    change whitenoise config

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -170,7 +170,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
